99db200ff5da9f15667456eb9d1aa031ad3a69d2
Roberto Carlos <betocarlos00@hotmail.com.br>
2025-08-30 21:11:14 -0300

    início atividades router

---
+++ Udemy/projetos-matheus/react-router/src/App.js
@@ -0,0 +1,35 @@
+import './App.css';
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Navbar from './components/Navbar';
+import Product from "./pages/Product";
+import Info from "./pages/Info";
+import NotFound from "./pages/NotFound";
+import Search from "./pages/Search";
+import { SearchForm } from "./components/SearchForm";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Router</h1>
+      <BrowserRouter>
+        <Navbar></Navbar>
+        <SearchForm />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="/products/:id/info" element={<Info />} />
+          <Route path="/products/:id" element={<Product />} />
+          <Route path="/search" element={<Search />} />
+
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
